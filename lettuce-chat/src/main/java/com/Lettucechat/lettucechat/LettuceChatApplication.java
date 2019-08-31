0324065beb02ee8790796e3a94b704ec1a15f932
Roman Gebrehiwot <rttgg1@gmail.com>
2019-08-30 18:50:47 -0700

    created the project

---
+++ lettuce-chat/src/main/java/com/Lettucechat/lettucechat/LettuceChatApplication.java
@@ -0,0 +1,13 @@
+package com.Lettucechat.lettucechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LettuceChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LettuceChatApplication.class, args);
+	}
+
+}
