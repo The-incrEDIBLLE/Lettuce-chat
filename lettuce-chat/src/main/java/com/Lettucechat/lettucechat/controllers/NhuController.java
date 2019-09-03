a1bb2a622fe5e6a31c5ef7a523b346b61abe4c9a
Nhu Trinh <nqtrinh87@gmail.com>
2019-09-03 12:10:22 -0700

    added many to many users/chats and users/users, changed method get random user to filter out users already added

--- lettuce-chat/src/main/java/com/Lettucechat/lettucechat/controllers/NhuController.java
+++ lettuce-chat/src/main/java/com/Lettucechat/lettucechat/controllers/NhuController.java
@@ -1,11 +1,10 @@
 package com.Lettucechat.lettucechat.controllers;
 
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 public class NhuController {
 
+  // TODO: edit profile route
   @PutMapping("/profile")
   public void updateUser(long viewedUserId){
 
