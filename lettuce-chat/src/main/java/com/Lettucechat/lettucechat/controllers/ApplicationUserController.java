fa7359d226da34facbe1048b3dc6ede7b550c10e
Nhu Trinh <nqtrinh87@gmail.com>
2019-09-04 10:12:42 -0700

    move routes to application user controller

--- lettuce-chat/src/main/java/com/Lettucechat/lettucechat/controllers/ApplicationUserController.java
+++ lettuce-chat/src/main/java/com/Lettucechat/lettucechat/controllers/ApplicationUserController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.security.Principal;
@@ -59,4 +60,33 @@ public class ApplicationUserController {
     return "profile";
   }
 
+  @PutMapping("/profile/edit")
+  public String updateUser(Model m, Principal p, long viewedUserId, String firstName, String lastName,
+                           String password, String imgUrl, String bio, String dietaryRestriction){
+    ApplicationUser applicationUser = applicationUserRepository.findById(viewedUserId).get();
+    applicationUser.setFirstName(firstName);
+    applicationUser.setLastName(lastName);
+    applicationUser.setPassword(encoder.encode(password));
+    applicationUser.setImgUrl(imgUrl);
+    applicationUser.setBio(bio);
+    applicationUser.setDietaryRestriction(dietaryRestriction);
+    applicationUserRepository.save(applicationUser);
+    m.addAttribute("viewedUser", applicationUser);
+    m.addAttribute("user", p);
+    return "profile";
+  }
+
+  @GetMapping("/profile/edit")
+  public String getEditProfileForm(Principal p, Model m){
+    ApplicationUser applicationUser = applicationUserRepository.findByUsername(p.getName());
+    m.addAttribute("viewedUser", applicationUser);
+    return "editProfile";
+  }
+
+  // TODO: delete user and their chats
+//  @DeleteMapping("/profile")
+//  public RedirectView deleteUser(long viewedUserId){
+//
+//    return new RedirectView("/logout");
+//  }
 }
