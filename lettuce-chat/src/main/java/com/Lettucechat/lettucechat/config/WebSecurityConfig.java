048bafd1fc8f1dbb63fcfdf1b91cbe534ad63df1
Nhu Trinh <nqtrinh87@gmail.com>
2019-09-04 22:17:19 -0700

    background, styling for all pages, added footer on all

--- lettuce-chat/src/main/java/com/Lettucechat/lettucechat/config/WebSecurityConfig.java
+++ lettuce-chat/src/main/java/com/Lettucechat/lettucechat/config/WebSecurityConfig.java
@@ -35,7 +35,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .cors().disable()
         .csrf().disable()
         .authorizeRequests()
-          .antMatchers("/", "/login", "/signup", "/error", "/style.css", "*.jpg", "/images/**", "/modal.css").permitAll()
+          .antMatchers("/", "/login", "/signup", "/error", "/style.css", "*.jpg", "/images/**", "/modal.css",
+              "/logout").permitAll()
           .antMatchers(HttpMethod.POST, "/users").permitAll()
           .anyRequest().authenticated()
         .and()
